db34fb0fc0e926bda89a126774d4862b6f3c0efb
Sergey Fedotov <sergfedotov@icloud.com>
2020-04-11 18:25:40 +0300

    CoreDataViewController

---
+++ homework14/homework14/CoreData/CoreDataViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataViewController.swift
+//  homework14
+//
+//  Created by Serg Fedotov on 11.04.2020.
+//  Copyright © 2020 Sergey Fedotov. All rights reserved.
+//
+
+import UIKit
+
+class CoreDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ homework14/homework14/Realm/RealmCell.swift
@@ -0,0 +1,38 @@
+//
+//  RealmCell.swift
+//  homework14
+//
+//  Created by Serg Fedotov on 11.04.2020.
+//  Copyright © 2020 Sergey Fedotov. All rights reserved.
+//
+
+import UIKit
+
+class RealmCell: UITableViewCell {
+    
+    let forminfo = AddFormConfig()
+    
+    @IBOutlet weak var NameSurname: UILabel!
+    @IBOutlet weak var DateTime: UILabel!
+    @IBOutlet weak var TaskText: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        let name = self.forminfo.name
+        let surname = self.forminfo.surname
+        
+        if ((name?.isEmpty) == nil) && ((surname?.isEmpty) == nil) {
+            NameSurname.text = "anonymous"
+        } else if ((name?.isEmpty) != nil) && ((surname?.isEmpty) == nil) {
+            NameSurname.text = "\(name!)"
+        } else if ((name?.isEmpty) == nil) && ((surname?.isEmpty) != nil) {
+            NameSurname.text = "\(surname!)"
+        } else {
+            NameSurname.text = "\(name!) \(surname!)"
+        }
+    }
+}
