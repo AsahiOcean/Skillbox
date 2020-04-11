2487ec32f26890c5145aded93aa9a4b3373a7ada
Sergey Fedotov <sergfedotov@icloud.com>
2020-04-11 16:58:06 +0300

    RealmViewController

---
+++ homework14/homework14/Realm/RealmViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RealmViewController.swift
+//  homework14
+//
+//  Created by Serg Fedotov on 11.04.2020.
+//  Copyright © 2020 Sergey Fedotov. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class RealmViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
--- homework14/homework14/UserDefaults/UserDefaultsViewController.swift
+++ homework14/homework14/UserDefaults/UserDefaultsViewController.swift
@@ -9,18 +9,22 @@
 import UIKit
 
 class UserDefaultsViewController: UIViewController {
-    
+
     @IBOutlet weak var Name: UITextField!
     @IBOutlet weak var Surname: UITextField!
+    @IBOutlet weak var GagView: UIView!
+    @IBOutlet weak var Welcome: UILabel!
+    @IBOutlet weak var RealmImage: UIImageView!
+    @IBOutlet weak var CoreDataImage: UIImageView!
+    @IBOutlet weak var RealmSection: UIButton!
+    @IBOutlet weak var CoreDataSection: UIButton!
     
     @IBAction func NameEdit(_ sender: Any) {
-        UserDataPersistance.sharing.NameData = Name.text
+        UserDataPersistance.sharing.NameData = Name.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
     }
-    
     @IBAction func SurnameEdit(_ sender: Any) {
-        UserDataPersistance.sharing.SurnameData = Surname.text
+        UserDataPersistance.sharing.SurnameData = Surname.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
     }
-    
     @IBAction func NameCleaner(_ sender: Any) {
         (Name.text, Surname.text) = (nil, nil)
         UserDataPersistance.sharing.NameData = nil
@@ -28,6 +32,15 @@ class UserDefaultsViewController: UIViewController {
         transitionFlipFromTop(Name)
         transitionFlipFromBottom(Surname)
         Name.becomeFirstResponder() // фокус на поле Name
+        Hello()
+    }
+    
+    @IBAction func RealmTap(_ sender: Any) {
+        disableAnimationImage()
+    }
+    
+    @IBAction func CoreDataTap(_ sender: Any) {
+        disableAnimationImage()
     }
     
     @IBAction func Exit(_ sender: Any) {
@@ -36,10 +49,12 @@ class UserDefaultsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        RealmSection.layer.zPosition = 1
+        CoreDataSection.layer.zPosition = 1
     }
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+
         if UserDataPersistance.sharing.NameData != nil {
             transitionFlipFromTop(Name)
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
@@ -52,5 +67,6 @@ class UserDefaultsViewController: UIViewController {
                 self.Surname.text = UserDataPersistance.sharing.SurnameData
             }
         }
+        Hello()
     }
 }
