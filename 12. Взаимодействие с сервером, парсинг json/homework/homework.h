e462f083000ee92ff44f1c972e9590d18b852825
AsahiOcean <sergfedotov@icloud.com>
2020-04-17 19:39:05 +0300

    Skillbox12

---
+++ 12. Взаимодействие с сервером, парсинг json/homework/homework.h
@@ -0,0 +1,19 @@
+//
+//  homework.h
+//  homework
+//
+//  Created by Serg Fedotov on 29.03.2020.
+//  Copyright © 2020 Sergey Fedotov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for homework.
+FOUNDATION_EXPORT double homeworkVersionNumber;
+
+//! Project version string for homework.
+FOUNDATION_EXPORT const unsigned char homeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <homework/PublicHeader.h>
+
+
