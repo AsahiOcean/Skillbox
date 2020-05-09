5eef13667ecf6ea6af26c4f861f3d4776a1a2ad5
AsahiOcean <sergfedotov@icloud.com>
2020-05-09 10:50:58 +0300

    Skillbox Finance

---
+++ IOS-разработка для начинающих/Skillbox Finance/BackgroundVideo/VideoPlayer.swift
@@ -0,0 +1,16 @@
+import Foundation
+import UIKit
+import AVKit
+
+func BackgroundVideo() -> UIView {
+    let background = UIView()
+    let path = URL(fileURLWithPath: Bundle.main.path(forResource: "background", ofType: "mov")!)
+    let player = AVPlayer(url: path)
+    let movie = AVPlayerLayer(player: player)
+    movie.frame = UIScreen.main.bounds
+    movie.videoGravity = AVLayerVideoGravity.resizeAspectFill
+    player.play()
+    background.layer.zPosition = -1
+    background.layer.addSublayer(movie)
+    return background
+}
