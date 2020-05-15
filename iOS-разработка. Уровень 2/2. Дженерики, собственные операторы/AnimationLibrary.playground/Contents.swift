2da8d2df966fb6886663fd870db4752bf7a45264
AsahiOcean <sergfedotov@icloud.com>
2020-05-15 14:17:15 +0300

    AnimationLibrary

---
+++ iOS-разработка. Уровень 2/2. Дженерики, собственные операторы/AnimationLibrary.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+import Foundation
+
+let view = UIView()
+    
+UIView.animate(withDuration: 0.3) {
+    view.alpha = 0
+}
+
+protocol Animator {
+    associatedtype Target
+    associatedtype Value
+ 
+    init(_ value: Value)
+    
+    func animate(target: Target)
+}
+
+class AlphaAnimator: Animator {
+
+    let newValue: CGFloat
+    
+    required init(_ value: CGFloat) {
+        newValue = value
+    }
+    
+    func animate(target: UIView) {
+        UIView.animate(withDuration: 0.3) {
+            target.alpha = 0
+        }
+    }
+}
+
+infix operator ~>
+func ~><T: Animator>(left: T, right: T.Target) {
+    left.animate(target: right)
+}
+AlphaAnimator(0) ~> view
