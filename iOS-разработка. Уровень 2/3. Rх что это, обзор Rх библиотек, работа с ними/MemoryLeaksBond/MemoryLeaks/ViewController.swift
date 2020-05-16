5f66e2d9af7452e834cde6a779d105902475d66e
AsahiOcean <sergfedotov@icloud.com>
2020-05-16 18:39:47 +0300

    MemoryLeaksBond

---
+++ iOS-разработка. Уровень 2/3. Rх что это, обзор Rх библиотек, работа с ними/MemoryLeaksBond/MemoryLeaks/ViewController.swift
@@ -0,0 +1,53 @@
+import UIKit
+import Bond
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var addButton: UIButton!
+    var processor: (() -> ())?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        addButton.reactive.tap.observeNext { [ weak self] in
+//            self?.dateprint()
+//        }
+        
+        // unowned - если мы уверены, что self не будет nil
+        addButton.reactive.tap.observeNext {
+            self.dateprint()
+        }.dispose(in: reactive.bag)
+        
+//        processor = {
+//            self.dateprint()
+//        }
+        
+        let h1 = Human()
+        let h2 = Human()
+        
+        h1.friend = h2
+        h2.friend = h1
+    }
+    
+    func dateprint() {
+        print(Date())
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        // тогда можно не писать [ unowned self] in
+        // удаляет reactive.bag
+        reactive.bag.dispose()
+    }
+}
+
+class Human {
+    weak var friend: Human?
+    
+    init() {
+        print("Human +++++")
+    }
+    
+    deinit {
+        print("Human -----")
+    }
+}
