f8bdf7516d6678951095c4bb8d66dd23f3c49714
AsahiOcean <sergfedotov@icloud.com>
2020-05-26 19:41:39 +0300

    Stack & Queue

---
+++ iOS-разработка. Уровень 2/7. Алгоритмы и структуры/Stack & Queue.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+// для стэка - мы добавляем элемент в конец и из конца их забираем
+class Stack<T> {
+    private var array: [T]
+    
+    init(elements: [T]) {
+        array = elements
+    }
+    
+    func push(element: T) {
+        array.append(element)
+    }
+    
+    func pop() -> T? {
+        array.removeLast()
+    }
+}
+
+// для очереди - добавляем элемент в конец, а забираем из начала списка
+class Queue<T> {
+    private var array: [T]
+    
+    init(elements: [T]) {
+        array = elements
+    }
+    
+    func push(element: T) {
+        array.append(element)
+    }
+    
+    func pop() -> T? {
+        array.removeFirst()
+    }
+}
