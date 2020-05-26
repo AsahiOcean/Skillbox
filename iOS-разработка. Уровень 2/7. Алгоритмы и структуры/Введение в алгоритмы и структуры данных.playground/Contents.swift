ae998d258e6bc9b29f82599d9a5dfe56c2cb1c99
AsahiOcean <sergfedotov@icloud.com>
2020-05-26 19:34:11 +0300

    Алгоритмы и структуры

---
+++ iOS-разработка. Уровень 2/7. Алгоритмы и структуры/Введение в алгоритмы и структуры данных.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+// LinkedList
+class LLNode<T> {
+    var key: T?
+    var next: LLNode<T>?
+    var previous: LLNode<T>?
+    
+    init(value: T?) {
+        key = value
+    }
+    
+    // convenience - чтобы можно было вызвать собственный конструктор
+    convenience init(values: [T]) {
+        self.init(value: values.first)
+        var current = self
+        for i in 1..<values.count {
+            let next = LLNode(value: values[i])
+            current.next = next
+            current = next as! Self
+            
+        }
+    }
+}
+
+let list = LLNode(values: [1,2,3,4,5])
+list.key
+list.next?.key
+list.next?.next?.key
