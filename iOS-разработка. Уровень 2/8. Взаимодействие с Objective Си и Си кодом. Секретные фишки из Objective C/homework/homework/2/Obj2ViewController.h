d770d83f801a68325ad7d189151277edd4559b89
AsahiOcean <asahiocean@icloud.com>
2020-08-28 03:51:38 +0500

    6q6luy79awkxnaz4

--- iOS-разработка. Уровень 2/8. Взаимодействие с Objective Си и Си кодом. Секретные фишки из Objective C/homework/homework/2/Obj2ViewController.h
+++ iOS-разработка. Уровень 2/8. Взаимодействие с Objective Си и Си кодом. Секретные фишки из Objective C/homework/homework/2/Obj2ViewController.h
@@ -1,11 +1,3 @@
-//
-//  Obj2ViewController.h
-//  homework
-//
-//  Created by Serg Fedotov on 14.06.2020.
-//  Copyright © 2020 Sergey Fedotov. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
