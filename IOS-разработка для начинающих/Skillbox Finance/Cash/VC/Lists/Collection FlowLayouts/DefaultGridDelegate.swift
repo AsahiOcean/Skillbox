ad89e0db61ff2122f20652c8ccccae3e5d2f8989
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 02:31:15 +0500

    1609104672

--- IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Lists/Collection FlowLayouts/DefaultGridDelegate.swift
+++ IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Lists/Collection FlowLayouts/DefaultGridDelegate.swift
@@ -3,31 +3,31 @@ import UIKit
 class DefaultGridDelegate: ItemsDefGridDelegate {
     private let itemsPerRow: CGFloat = 3
     private let space: CGFloat = 8
-
+    
     func collectionView(_ collectionView: UICollectionView,
-    layout collectionViewLayout: UICollectionViewLayout,
-    sizeForItemAt indexPath: IndexPath) -> CGSize {
-    let indent = sectionInsets.left + sectionInsets.right + space * (itemsPerRow - 1)
-    let availableWidth = collectionView.bounds.width - indent
-    let widthPerItem = availableWidth / itemsPerRow
-    return CGSize(width: widthPerItem, height: widthPerItem)
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let indent = sectionInsets.left + sectionInsets.right + space * (itemsPerRow - 1)
+        let availableWidth = collectionView.bounds.width - indent
+        let widthPerItem = availableWidth / itemsPerRow
+        return CGSize(width: widthPerItem, height: widthPerItem)
     }
     
     func collectionView(_ collectionView: UICollectionView,
-    layout collectionViewLayout: UICollectionViewLayout,
-    insetForSectionAt section: Int) -> UIEdgeInsets {
-    return sectionInsets
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
     }
     
     func collectionView(_ collectionView: UICollectionView,
-    layout collectionViewLayout: UICollectionViewLayout,
-    minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-    return 20
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
     }
     
     func collectionView(_ collectionView: UICollectionView,
-    layout collectionViewLayout: UICollectionViewLayout,
-    minimumInteritemSpacingForSectionAt section: Int) -> CGFloat{
-    return space
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat{
+        return space
     }
 }
