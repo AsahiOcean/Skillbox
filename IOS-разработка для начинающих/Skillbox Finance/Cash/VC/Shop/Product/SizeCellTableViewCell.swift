7f4ea0b19bcb3c2f7f1cc1725e1c0e5447903518
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 02:37:59 +0500

    Update SizeCellTableViewCell.swift

--- IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Shop/Product/SizeCellTableViewCell.swift
+++ IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Shop/Product/SizeCellTableViewCell.swift
@@ -3,16 +3,12 @@ import UIKit
 class SizeCellTableViewCell: UITableViewCell {
     
     @IBOutlet weak var sizeLabel: UILabel!
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-
 }
