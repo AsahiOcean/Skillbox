1f505650ce6190debd63237dc859f0c367c04f08
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 02:04:16 +0500

    Update homework.h

--- IOS-разработка для начинающих/12. Взаимодействие с сервером, парсинг json/homework/homework.h
+++ IOS-разработка для начинающих/12. Взаимодействие с сервером, парсинг json/homework/homework.h
@@ -7,5 +7,3 @@ FOUNDATION_EXPORT double homeworkVersionNumber;
 FOUNDATION_EXPORT const unsigned char homeworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <homework/PublicHeader.h>
-
-
