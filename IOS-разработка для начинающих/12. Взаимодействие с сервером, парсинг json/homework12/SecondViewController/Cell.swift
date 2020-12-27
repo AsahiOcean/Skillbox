dfa505068b92f5f135957cee31bd26c073cddf1f
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 02:02:27 +0500

    Update Cell.swift

--- IOS-разработка для начинающих/12. Взаимодействие с сервером, парсинг json/homework12/SecondViewController/Cell.swift
+++ IOS-разработка для начинающих/12. Взаимодействие с сервером, парсинг json/homework12/SecondViewController/Cell.swift
@@ -1,14 +1,10 @@
 import UIKit
-// Skillbox
-// Скиллбокс
 
 class Cell: UITableViewCell {
-    
     @IBOutlet weak var TimeLabel: UILabel!
     @IBOutlet weak var TempLabel: UILabel!
     @IBOutlet weak var WindLabel: UILabel!
     @IBOutlet weak var OsadkiLabel: UILabel!
     @IBOutlet weak var HumidityLabel: UILabel!
     @IBOutlet weak var PressureLabel: UILabel!
-    
 }
