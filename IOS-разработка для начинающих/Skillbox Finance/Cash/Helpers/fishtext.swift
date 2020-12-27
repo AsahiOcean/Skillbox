ef6e44307d146edde9744b13f1da43b01736fb86
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 02:47:46 +0500

    Update fishtext.swift

--- IOS-разработка для начинающих/Skillbox Finance/Cash/Helpers/fishtext.swift
+++ IOS-разработка для начинающих/Skillbox Finance/Cash/Helpers/fishtext.swift
@@ -1,4 +1,3 @@
-import Foundation
 import UIKit
 import Alamofire
 import RealmSwift
@@ -7,7 +6,7 @@ import SwiftyJSON
 func fishtext(tf: UITextView) {
     AF.request("https://fish-text.ru/get?format=html&number=3").response { response in
         if let data = response.data, let utf8 = String(data: data, encoding: .utf8) {
-    tf.text = utf8.components(separatedBy: ["<", ">", "p", "/"]).joined().trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+            tf.text = utf8.components(separatedBy: ["<", ">", "p", "/"]).joined().trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
         }
     }
 }
@@ -15,15 +14,15 @@ func fishtext(tf: UITextView) {
 func NewsArray() {
     AF.request("https://fish-text.ru/get?format=html&number=3").response { response in
         if let data = response.data,
-    let utf8 = String(data: data, encoding: .utf8) {
-    TextArray.append(utf8.components(separatedBy: ["<", ">", "p", "/"]).joined().trimmingCharacters(in: CharacterSet.whitespacesAndNewlines))
+           let utf8 = String(data: data, encoding: .utf8) {
+            TextArray.append(utf8.components(separatedBy: ["<", ">", "p", "/"]).joined().trimmingCharacters(in: CharacterSet.whitespacesAndNewlines))
         }
     }
     
     AF.request("https://fish-text.ru/get?format=html&number=1").response { response in
         if let data = response.data,
-    let utf8 = String(data: data, encoding: .utf8) {
-    EventText.append(utf8.components(separatedBy: ["<", ">", "p", "/"]).joined().trimmingCharacters(in: CharacterSet.whitespacesAndNewlines))
+           let utf8 = String(data: data, encoding: .utf8) {
+            EventText.append(utf8.components(separatedBy: ["<", ">", "p", "/"]).joined().trimmingCharacters(in: CharacterSet.whitespacesAndNewlines))
         }
     }
     print("«Новости» скачаны в TextArray")
@@ -32,19 +31,19 @@ func NewsArray() {
 
 /// Realm + JSON, https://blockchain.info/ru/ticker
 func BitcoinRate() {
-let realm = try! Realm()
-let rate = realm.objects(BitCoinObject.self)
+    let realm = try! Realm()
+    let rate = realm.objects(BitCoinObject.self)
     AF.request("https://blockchain.info/ru/ticker").responseJSON { response in
         let status = response.response?.statusCode
         if status == 200 {
-        Persistance().addBitCoinRate()
-        let json = try! JSON(data: response.data!)
-        if let rate = rate.last {
-            try! realm.write {
-            rate.USD = json["USD"]["buy"].int ?? 0
-            rate.EUR = json["EUR"]["buy"].int ?? 0
-            rate.RUB = json["RUB"]["buy"].int ?? 0
-        }}
+            Persistance().addBitCoinRate()
+            let json = try! JSON(data: response.data!)
+            if let rate = rate.last {
+                try! realm.write {
+                    rate.USD = json["USD"]["buy"].int ?? 0
+                    rate.EUR = json["EUR"]["buy"].int ?? 0
+                    rate.RUB = json["RUB"]["buy"].int ?? 0
+                }}
         } else {
             print("!= 200")
         }
@@ -56,4 +55,4 @@ let rate = realm.objects(BitCoinObject.self)
  LOGIN: rapsucerka@enayu.com
  PASS: 1q2w3e4r5t6y
  API: 0b3acc09a10e4ee4a4f468ee9061e6e1
-*/
+ */
