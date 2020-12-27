1f80e41fcef24d5cc83bf7a8278a888d1e56e70e
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 02:28:05 +0500

    Update Page.swift

--- IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Lists/PageController/Page.swift
+++ IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Lists/PageController/Page.swift
@@ -5,12 +5,12 @@ class PageViewController: UIViewController {
     @IBOutlet weak var PagerScroll: UIScrollView!
     
     var PageViewController: PageController? {
-    didSet { PageViewController?.PageDelegate = self }
+        didSet { PageViewController?.PageDelegate = self }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        pageControl.addTarget(self, action: Selector("didChangePageControlValue"), for: .valueChanged)
+        //        pageControl.addTarget(self, action: Selector("didChangePageControlValue"), for: .valueChanged)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -18,26 +18,26 @@ class PageViewController: UIViewController {
             self.PageViewController = PageViewController
         }
     }
-
+    
     @IBAction func didTapNextButton(_ sender: Any) {
         PageViewController?.scrollToNextViewController()
     }
-
+    
     func didChangePageControlValue() {
-//        PageViewController?.scrollToViewController(index: pageControl.currentPage)
+        //        PageViewController?.scrollToViewController(index: pageControl.currentPage)
     }
 }
 
 extension PageViewController: PageControllerDelegate {
     func PageViewController(PageViewController: PageController,
-        didUpdatePageCount count: Int) {
-//        pageControl.numberOfPages = count
+                            didUpdatePageCount count: Int) {
+        //        pageControl.numberOfPages = count
         PagerScroll.frame = CGRect(x: 0, y: 88, width: self.view.frame.width, height: 40)
     }
     
     func PageViewController(PageViewController: PageController,
-        didUpdatePageIndex index: Int) {
+                            didUpdatePageIndex index: Int) {
         print(index)
-//        pageControl.currentPage = index
+        //        pageControl.currentPage = index
     }
 }
