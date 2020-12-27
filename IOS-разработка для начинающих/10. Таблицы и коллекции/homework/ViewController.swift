d0c53d7118b8bd640a8f41c50415b419ea9c6224
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 01:53:23 +0500

    Update ViewController.swift

--- IOS-разработка для начинающих/10. Таблицы и коллекции/homework/ViewController.swift
+++ IOS-разработка для начинающих/10. Таблицы и коллекции/homework/ViewController.swift
@@ -1,42 +1,41 @@
 /*
  Для всех задач сделать один проект, в котором все задачи будут реализованы на отдельных экранах.
-
+ 
  1. Создать UICollectionView со всеми элементами, как показано на картинке. При запуске должно показываться 6 ячеек.
-
+ 
  2. Создать UITableView с ячейками, идентичную скриншоту настроек.
-
+ 
  3. Создать UIScrollView, в которой будет аналогичный контент, как на скриншоте. Контент должен адаптироваться к ширине экрана (сохранять отступы). Дизайнить все элементы один в один как на скриншоте не нужно, можно просто обозначить их цветными блоками.
-
+ 
  Ключевое в этой задаче — констрейнты между элементами и скролл вью.
  */
-// Skillbox
-// Скиллбокс
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var ItemsArray = ["Ботинки", "Пальто", "Джинсы", "Пуховик", "Рубашка", "Брюки", "Бомбер", "Плащ", "Парка", "Кроссовки", "Cвитер", "Толстовка", "Жилет", "Поло", "Чиносы", "Ботинки", "Пальто", "Джинсы", "Пуховик", "Рубашка", "Брюки", "Бомбер", "Плащ", "Парка", "Кроссовки", "Cвитер", "Толстовка", "Жилет", "Поло", "Чиносы" ]
-
+    
     @IBOutlet weak var CollectionView: UICollectionView!
     
-// Navigation Bar
+    // Navigation Bar
     @IBOutlet weak var NavigationBarOutlet: UINavigationBar!
     @IBOutlet weak var NavigationBarLabel: UINavigationItem!
     
-// Строка поиска // Search // SearchBar // SearchLine
+    // Строка поиска // Search // SearchBar // SearchLine
     @IBOutlet weak var SearchLineTextField: UITextField!
-
-// Кнопки
+    
+    // Кнопки
     @IBOutlet weak var SearchButton: UIButton! // поиск
     @IBAction func SearchTap(_ sender: Any) {
         print("Нажата кнопка Поиск")
     }
-
+    
     @IBOutlet weak var FilterButton: UIButton! // фильтр
     @IBAction func FilterTap(_ sender: Any) {
         print("Нажата кнопка Фильтры")
     }
-
+    
     @IBOutlet weak var TagButton: UIButton! // вкладка 1
     @IBAction func TagTap(_ sender: Any) {
         print("Нажата первая вкладка")
@@ -46,12 +45,12 @@ class ViewController: UIViewController {
     @IBAction func ShirtTap(_ sender: Any) {
         print("Нажата вторая вкладка")
     }
-
+    
     @IBOutlet weak var HouseButton: UIButton! // вкладка 3
     @IBAction func HouseTap(_ sender: Any) {
         print("Нажата третья вкладка")
     }
-
+    
     @IBOutlet weak var BagButton: UIButton! // вкладка 4
     @IBAction func BagTap(_ sender: Any) {
         print("Нажата четвертая вкладка")
@@ -61,12 +60,12 @@ class ViewController: UIViewController {
     @IBAction func AccountTap(_ sender: Any) {
         print("Нажата пятая вкладка")
     }
-
-// Для подсчета ширины ячейки под соотношение сторон экрана
-// Например для айпада
-
+    
+    // Для подсчета ширины ячейки под соотношение сторон экрана
+    // Например для айпада
+    
     var RatioForWidth: CGFloat = (UIScreen.main.bounds.height / UIScreen.main.bounds.width) > 1.5 ? 2 : 3
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -77,22 +76,22 @@ extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDa
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return ItemsArray.count
     }
-
-// Размер ячейки
+    
+    // Размер ячейки
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: ((view.frame.size.width - 25) / RatioForWidth), height: (view.frame.size.height / 3))
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ItemCell", for: indexPath) as! CollectionViewCell
-
-// Генератор фигни
+        
+        // Генератор фигни
         let OldPrice = (arc4random() % 100000 < 19990 ? 9990 : UInt32(29990))
         let Skidon = (arc4random() % 100) >= 50 ? arc4random() % 50 : +(arc4random() % 50)
         let NewPrice = (OldPrice - ((OldPrice / 100) * Skidon))
         let ItemName = ItemsArray.randomElement()
-
-// Вывод
+        
+        // Вывод
         cell.ItemImageView.image = UIImage.init(named: ItemName!)
         cell.OldPriceLabel.text = String(OldPrice) + " ₽"
         cell.NewPriceLabel.text = String(NewPrice <= 9990 ? "Распродано" : String(NewPrice) + " ₽")
