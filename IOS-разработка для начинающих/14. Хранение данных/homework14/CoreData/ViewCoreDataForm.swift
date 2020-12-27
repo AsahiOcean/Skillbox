d6850e10b953ccc0ff80f49dffe52227df9b887e
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 02:10:46 +0500

    Update ViewCoreDataForm.swift

--- IOS-разработка для начинающих/14. Хранение данных/homework14/CoreData/ViewCoreDataForm.swift
+++ IOS-разработка для начинающих/14. Хранение данных/homework14/CoreData/ViewCoreDataForm.swift
@@ -2,8 +2,6 @@ import UIKit
 import CoreData
 import Alamofire
 import SVProgressHUD
-// Skillbox
-// Скиллбокс
 
 class ViewCoreDataForm: UIView {
     
@@ -12,49 +10,49 @@ class ViewCoreDataForm: UIView {
     fileprivate let general = CoreDataViewController()
     fileprivate let name = UserDataPersistance.sharing.NameData
     fileprivate let surname = UserDataPersistance.sharing.SurnameData
-
+    
     @IBOutlet weak var YesButton: UIButton!
     @IBOutlet weak var NoButton: UIButton!
     @IBOutlet weak var Assistant: UIButton!
     @IBOutlet weak var TaskText: UITextView!
     
     // MARK: - Buttons
-
+    
     @IBAction func YesTap(_ sender: Any) {
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         let task = Tasks(context: context)
-            task.name = username()
-            task.date = date()
-            task.text = TaskText.text
-
+        task.name = username()
+        task.date = date()
+        task.text = TaskText.text
+        
         // передать присвоенные значения
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
-
-            print("- - - - - - - - - - - - - - - - - - - - - ")
-            print("Время: \(date())")
-            print("UUID: \(NSUUID().uuidString.lowercased())")
-            print("Пользователь: \(self.username())")
-            print("\(self.TaskText.text ?? "* ПУСТО *")")
-            print("- - - - - - - - - - - - - - - - - - - - - ")
-
+        
+        print("- - - - - - - - - - - - - - - - - - - - - ")
+        print("Время: \(date())")
+        print("UUID: \(NSUUID().uuidString.lowercased())")
+        print("Пользователь: \(self.username())")
+        print("\(self.TaskText.text ?? "* ПУСТО *")")
+        print("- - - - - - - - - - - - - - - - - - - - - ")
+        
         UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
-                super.alpha = 0
-                super.center.y -= UIScreen.main.bounds.height
-            }, completion: { _ in
-                super.removeFromSuperview()
+            super.alpha = 0
+            super.center.y -= UIScreen.main.bounds.height
+        }, completion: { _ in
+            super.removeFromSuperview()
         })
     }
     
     @IBAction func NoTap(_ sender: Any) {
         UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
-                super.alpha = 0
-                super.center.y += UIScreen.main.bounds.height
-            }, completion: { _ in
-                super.removeFromSuperview()
-                print("Порадуйте новостями...")
+            super.alpha = 0
+            super.center.y += UIScreen.main.bounds.height
+        }, completion: { _ in
+            super.removeFromSuperview()
+            print("Порадуйте новостями...")
         })
     }
-
+    
     @IBAction func FishTextGen(_ sender: Any) {
         SVProgressHUD.show(withStatus:"Придумываю...")
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
@@ -62,14 +60,14 @@ class ViewCoreDataForm: UIView {
         }
         SVProgressHUD.dismiss(withDelay: 1.0)
     }
-
-// MARK: - loadFromNIB
+    
+    // MARK: - loadFromNIB
     static func loadFromNIB() -> ViewCoreDataForm {
-            let nib = UINib(nibName: "AddFormCoreData", bundle: nil)
-            return nib.instantiate(withOwner: self, options: nil).first as! ViewCoreDataForm
+        let nib = UINib(nibName: "AddFormCoreData", bundle: nil)
+        return nib.instantiate(withOwner: self, options: nil).first as! ViewCoreDataForm
     }
     
-// MARK: - username check
+    // MARK: - username check
     func username() -> String {
         if ((name?.isEmpty) == nil) && ((surname?.isEmpty) == nil) {
             return "anonymous"
@@ -81,7 +79,7 @@ class ViewCoreDataForm: UIView {
             return "\(name!) \(surname!)"
         }
     }
-// MARK: - Date
+    // MARK: - Date
     func date() -> String {
         let date = Date()
         let dateFormatter = DateFormatter()
@@ -92,7 +90,7 @@ class ViewCoreDataForm: UIView {
         return strTime
     }
     
-// MARK: - TextGenerator
+    // MARK: - TextGenerator
     func TextGenerator() {
         AF.request("https://fish-text.ru/get?format=html&number=2").response { response in
             if let data = response.data, let utf8 = String(data: data, encoding: .utf8) {
@@ -100,9 +98,9 @@ class ViewCoreDataForm: UIView {
             } else {
                 SVProgressHUD.showError(withStatus: "Помните...")
                 DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                self.TaskText.textAlignment = .center
-                self.TaskText.font = .systemFont(ofSize: 22)
-                self.TaskText.text = "«У всего есть чудеса свои чудеса, даже у темноты и тишины - учитесь быть довольными независимо от того, где находитесь».\n\nХелен Адамс Келлер."
+                    self.TaskText.textAlignment = .center
+                    self.TaskText.font = .systemFont(ofSize: 22)
+                    self.TaskText.text = "«У всего есть чудеса свои чудеса, даже у темноты и тишины - учитесь быть довольными независимо от того, где находитесь».\n\nХелен Адамс Келлер."
                 }
                 SVProgressHUD.dismiss(withDelay: 1.0)
             }
