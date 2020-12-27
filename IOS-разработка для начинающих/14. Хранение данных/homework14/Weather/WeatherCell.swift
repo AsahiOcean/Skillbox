7138f823e4310cbc60b1bf4acfa9bf5c99a4ff6e
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 02:09:55 +0500

    Update WeatherCell.swift

--- IOS-разработка для начинающих/14. Хранение данных/homework14/Weather/WeatherCell.swift
+++ IOS-разработка для начинающих/14. Хранение данных/homework14/Weather/WeatherCell.swift
@@ -1,16 +1,10 @@
 import UIKit
-// Skillbox
-// Скиллбокс
 
 class WeatherCell: UITableViewCell {
     
     @IBOutlet weak var Date: UILabel!
     @IBOutlet weak var temperature: UILabel!
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
