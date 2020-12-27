f0c38cd086f3e5c8dc53dabb30c0f1902aaba194
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 01:58:22 +0500

    Update Clock.swift

--- IOS-разработка для начинающих/11. Создание собственных UI компонентов/homework11/Clock.swift
+++ IOS-разработка для начинающих/11. Создание собственных UI компонентов/homework11/Clock.swift
@@ -1,43 +1,41 @@
 /*
-  Часы с часовой, минутной и секундной стрелкой, у которых можно менять цвет и размер каждой стрелки
- */
-// Skillbox
-// Скиллбокс
+ Часы с часовой, минутной и секундной стрелкой, у которых можно менять цвет и размер каждой стрелки
+*/
 
 import UIKit
 
 @IBDesignable class Clock: UIView {
-
+    
     var isSetuped = false
     
-// MARK: - Стрелки
+    // MARK: - Стрелки
     
     private let HourHand = UIView()
     private let MinuteHand = UIView()
     private let SecondsHand = UIView()
-
-//  Ширина
+    
+    //  Ширина
     @IBInspectable var HourHandWidth: CGFloat = 6 {
-            didSet { _ = HourHand.layer.frame.width }
+        didSet { _ = HourHand.layer.frame.width }
     }
     @IBInspectable var MinuteHandWidth: CGFloat = 4 {
-            didSet { _ = MinuteHand.layer.frame.width }
+        didSet { _ = MinuteHand.layer.frame.width }
     }
     @IBInspectable var SecondsHandWidth: CGFloat = 2 {
-            didSet { _ = SecondsHand.layer.frame.width }
+        didSet { _ = SecondsHand.layer.frame.width }
     }
-// Длина
-// чем больше число - тем ниже стрелка
+    // Длина
+    // чем больше число - тем ниже стрелка
     @IBInspectable var HourHandHeight: CGFloat = 60 {
         didSet { _ = HourHand.layer.frame.height }
-        }
+    }
     @IBInspectable var MinuteHandHeight: CGFloat = 40 {
         didSet { _ = MinuteHand.layer.frame.height }
-        }
+    }
     @IBInspectable var SecondsHandHeight: CGFloat = 20 {
         didSet { _ = SecondsHand.layer.frame.height }
-        }
-//  Цвет
+    }
+    //  Цвет
     @IBInspectable var HourHandColor: UIColor = UIColor.green {
         didSet { HourHand.backgroundColor = HourHandColor }
     }
@@ -47,18 +45,18 @@ import UIKit
     @IBInspectable var SecondsHandColor: UIColor = UIColor.red {
         didSet { SecondsHand.backgroundColor = SecondsHandColor }
     }
-// MARK: - Деления циферблата
+    // MARK: - Деления циферблата
     let HourMarkerWidth: CGFloat = 5
     let HourMarkerHeight: CGFloat = 15
-// Цвет
+    // Цвет
     let HourMarkerColor: UIColor = UIColor.black
-// UIView
+    // UIView
     private let HourMarkerUp = UIView()
     private let HourMarkerLeft = UIView()
     private let HourMarkerRight = UIView()
     private let HourMarkerBottom = UIView()
     
-// MARK: - Сколько время?
+    // MARK: - Сколько время?
     @IBInspectable var Hours: CGFloat = 12 {
         didSet { layoutIfNeeded() }
     }
@@ -68,35 +66,35 @@ import UIKit
     @IBInspectable var Seconds: CGFloat = 60 {
         didSet { layoutIfNeeded() }
     }
-
-// MARK: - layoutSubviews
+    
+    // MARK: - layoutSubviews
     override func layoutSubviews() {
         super.layoutSubviews()
         
-// Размеры фрейма
+        // Размеры фрейма
         let W = frame.size.width
         let H = frame.size.height
-
-// Приоритет
+        
+        // Приоритет
         HourHand.layer.zPosition = 3
         MinuteHand.layer.zPosition = 2
         SecondsHand.layer.zPosition = 1
-
-// anchorPoint
+        
+        // anchorPoint
         HourHand.layer.anchorPoint = CGPoint(x: 0, y: 1)
         MinuteHand.layer.anchorPoint = CGPoint(x: 0, y: 1)
         SecondsHand.layer.anchorPoint = CGPoint(x: 0, y: 1)
-
-// СТРЕЛКИ: размеры и координаты
+        
+        // СТРЕЛКИ: размеры и координаты
         HourHand.frame = CGRect(x: W / 2 - HourHandWidth, y: HourHandHeight, width: HourHandWidth, height: H / 2 - HourHandHeight)
         MinuteHand.frame = CGRect(x: W / 2 - MinuteHandWidth, y: MinuteHandHeight, width: MinuteHandWidth, height: H / 2 - MinuteHandHeight )
         SecondsHand.frame = CGRect(x: W / 2 - SecondsHandWidth, y: SecondsHandHeight, width: SecondsHandWidth, height: H / 2 - SecondsHandHeight )
-// Цвет стрелок
+        // Цвет стрелок
         HourHand.backgroundColor = HourHandColor
         MinuteHand.backgroundColor = MinuteHandColor
         SecondsHand.backgroundColor = SecondsHandColor
-
-// Размер и позиция делений циферблата
+        
+        // Размер и позиция делений циферблата
         HourMarkerUp.frame = CGRect(x: W / 2 - HourMarkerWidth, y: 0, width: HourMarkerWidth, height: HourMarkerHeight)
         
         HourMarkerLeft.frame = CGRect(x: 0, y: H / 2 - HourMarkerWidth, width: HourMarkerHeight, height: HourMarkerWidth)
@@ -105,10 +103,10 @@ import UIKit
         
         HourMarkerBottom.frame = CGRect(x: H / 2 - HourMarkerWidth, y: H - HourMarkerHeight, width: HourMarkerWidth, height: HourMarkerHeight)
         
-// Круглый циферблат
+        // Круглый циферблат
         layer.cornerRadius = frame.size.width / 2
-
-// MARK: - Создаем элементы
+        
+        // MARK: - Создаем элементы
         for element in [HourMarkerUp, HourMarkerLeft, HourMarkerRight, HourMarkerBottom] {
             element.backgroundColor = HourMarkerColor
             addSubview(element)
