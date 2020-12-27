743238020167cf299466a52c1cf89e1371d6b9af
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 01:39:29 +0500

    Update AppDelegate.swift

--- IOS-разработка для начинающих/6. Xcode. Controller и сториборд/Homework/AppDelegate.swift
+++ IOS-разработка для начинающих/6. Xcode. Controller и сториборд/Homework/AppDelegate.swift
@@ -9,11 +9,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     // MARK: UISceneSession Lifecycle
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
     
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        
     }
 }
