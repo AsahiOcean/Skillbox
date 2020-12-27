765c2476f3a32289daa229e0fee14ca5ca44e644
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 02:45:47 +0500

    Update refillViewController.swift

--- IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Profile/refillViewController.swift
+++ IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Profile/refillViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 import RealmSwift
 
 class refillViewController: UIViewController {
-
+    
     @IBOutlet weak var plus: UILabel!
     @IBOutlet weak var balance: UILabel!
     
@@ -14,28 +14,14 @@ class refillViewController: UIViewController {
         let userinfo = self.realm.objects(UserInfo.self)
         plus.text = "\(userinfo.first!.pourboire)"
         balance.text = "\(userinfo.first!.money + Int(plus.text!)!)"
-//
-//        let money = Int(balance.text!)! + Int(plus.text!)!
-//
+        //
+        //        let money = Int(balance.text!)! + Int(plus.text!)!
+        //
         if let userinfo = userinfo.first {
             try! self.realm.write {
-            userinfo.money = Int(balance.text!)!
-            userinfo.pourboire = 0
-        }}
-
-}}
-
-
-        // Do any additional setup after loading the view.
-
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+                userinfo.money = Int(balance.text!)!
+                userinfo.pourboire = 0
+            }}
+        
     }
-    */
+}
