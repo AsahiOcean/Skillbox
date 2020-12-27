78b20b6f2e6e8a6245a93f6595798fe70e431bee
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 01:53:56 +0500

    Update CollectionViewCell.swift

--- IOS-разработка для начинающих/10. Таблицы и коллекции/homework/CollectionViewCell.swift
+++ IOS-разработка для начинающих/10. Таблицы и коллекции/homework/CollectionViewCell.swift
@@ -1,11 +1,9 @@
 import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
-
     @IBOutlet weak var ItemLabel: UILabel!
     @IBOutlet weak var NewPriceLabel: UILabel!
     @IBOutlet weak var OldPriceLabel: UILabel!
     @IBOutlet weak var ItemImageView: UIImageView!
     @IBOutlet weak var Skidon: UILabel!
-    
 }
