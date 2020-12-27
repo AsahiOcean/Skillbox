0930b8288c9cdbdf7b83a3d4e679bd49c971a573
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 01:54:58 +0500

    Update TableViewCell.swift

--- IOS-разработка для начинающих/10. Таблицы и коллекции/homework/TableViewCell.swift
+++ IOS-разработка для начинающих/10. Таблицы и коллекции/homework/TableViewCell.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var IconImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var Switch: UISwitch!
@@ -11,16 +11,16 @@ class TableViewCell: UITableViewCell {
             Switch.isOn == true ? print("Включен Авиарежим") : print("Авиарежим отключен")
         }
     }
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         Switch.isOn = false
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         if selected {
-             print("\(nameLabel.text!)")
+            print("\(nameLabel.text!)")
         }
     }
 }
