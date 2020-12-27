7c43b6da6df4ccf178b786cff289533244e35de4
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 02:31:50 +0500

    Update TableViewDelegate.swift

--- IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Lists/Collection FlowLayouts/TableViewDelegate.swift
+++ IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Lists/Collection FlowLayouts/TableViewDelegate.swift
@@ -1,24 +1,24 @@
 import UIKit
 
 class TableViewDelegate: ItemsDefGridDelegate {
-
+    
     func collectionView(_ collectionView: UICollectionView,
-    layout collectionViewLayout: UICollectionViewLayout,
-    sizeForItemAt indexPath: IndexPath) -> CGSize {
-    let indent = sectionInsets.left + sectionInsets.right
-    let widthPerItem = collectionView.bounds.width - indent
-    return CGSize(width: widthPerItem, height: 125)
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let indent = sectionInsets.left + sectionInsets.right
+        let widthPerItem = collectionView.bounds.width - indent
+        return CGSize(width: widthPerItem, height: 125)
     }
     
     func collectionView(_ collectionView: UICollectionView,
-    layout collectionViewLayout: UICollectionViewLayout,
-    insetForSectionAt section: Int) -> UIEdgeInsets {
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
         return sectionInsets
     }
     
     func collectionView(_ collectionView: UICollectionView,
-    layout collectionViewLayout: UICollectionViewLayout,
-    minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-    return 1
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
     }
 }
