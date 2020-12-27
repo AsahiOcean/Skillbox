89254a1ac1d6f2e97c915fefcc1bfbc82389b9af
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 02:41:47 +0500

    Update HistoryTableViewCell.swift

--- IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Profile/History/HistoryTableViewCell.swift
+++ IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Profile/History/HistoryTableViewCell.swift
@@ -6,16 +6,12 @@ class HistoryTableViewCell: UITableViewCell {
     @IBOutlet weak var colorLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var sizeLabel: UILabel!
-        
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
-
 }
