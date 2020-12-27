88f732d72863add4519e1a944296b938c82d10b9
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 02:35:02 +0500

    Update EmailVC.swift

--- IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Login & Reg/EmailVC.swift
+++ IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Login & Reg/EmailVC.swift
@@ -12,62 +12,62 @@ class EmailVC: UIViewController {
     var rateResult: Results<BitCoinObject>!
     
     @IBAction func FacebookTrue(_ sender: Any) {
-    let userinfo = self.realm.objects(UserInfo.self)
-    if let userinfo = userinfo.first {
-        try! self.realm.write {
-        userinfo.facebook = true
-/* https://developers.facebook.com/docs/facebook-login/overview/?locale=ru_RU
-*/
-    }}}
+        let userinfo = self.realm.objects(UserInfo.self)
+        if let userinfo = userinfo.first {
+            try! self.realm.write {
+                userinfo.facebook = true
+                /* https://developers.facebook.com/docs/facebook-login/overview/?locale=ru_RU
+                 */
+            }}}
     
     override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    let userinfo = self.realm.objects(UserInfo.self)
-    if (userinfo.first?.email.isEmpty)! { self.EmailPassGen()
-    } else { self.EmailLogin() }
-    
-    // Периферия
-    enter.layer.cornerRadius = 10
-    self.view.addSubview(BackgroundVideo())
+        super.viewDidLoad()
+        
+        let userinfo = self.realm.objects(UserInfo.self)
+        if (userinfo.first?.email.isEmpty)! { self.EmailPassGen()
+        } else { self.EmailLogin() }
+        
+        // Периферия
+        enter.layer.cornerRadius = 10
+        self.view.addSubview(BackgroundVideo())
     }
     
     fileprivate func EmailLogin() {
-    let userinfo = self.realm.objects(UserInfo.self)
-    asyncAfter(1.0) {
-        self.Email.text?.append("\(userinfo.first!.email)")
-    asyncAfter(1.0) {
-        self.Pass.text?.append("\(userinfo.first!.pass)")
-        self.enter.alpha = 1.0;
-    asyncAfter(0.5) { self.GoToLists() }
-    }}}
+        let userinfo = self.realm.objects(UserInfo.self)
+        asyncAfter(1.0) {
+            self.Email.text?.append("\(userinfo.first!.email)")
+            asyncAfter(1.0) {
+                self.Pass.text?.append("\(userinfo.first!.pass)")
+                self.enter.alpha = 1.0;
+                asyncAfter(0.5) { self.GoToLists() }
+            }}}
     
     /// Имитация ввода данных пользователем
     fileprivate func EmailPassGen() {
-    EmailGen(TF: self.Email)
-    let userinfo = self.realm.objects(UserInfo.self)
-    asyncAfter(2.5) {
-        if let userinfo = userinfo.first { try! self.realm.write {
-        userinfo.email = "\(self.Email.text!)" }
-        PassGen(TF: self.Pass)
-    asyncAfter(2.5) {
+        EmailGen(TF: self.Email)
         let userinfo = self.realm.objects(UserInfo.self)
-        if let userinfo = userinfo.first { try! self.realm.write {
-        userinfo.pass = "\(self.Pass.text!)"
-        self.enter.alpha = 1.0
-    asyncAfter(0.5) { self.GoToLists() }
-    }}}}}}
+        asyncAfter(2.5) {
+            if let userinfo = userinfo.first { try! self.realm.write {
+                userinfo.email = "\(self.Email.text!)" }
+                PassGen(TF: self.Pass)
+                asyncAfter(2.5) {
+                    let userinfo = self.realm.objects(UserInfo.self)
+                    if let userinfo = userinfo.first { try! self.realm.write {
+                        userinfo.pass = "\(self.Pass.text!)"
+                        self.enter.alpha = 1.0
+                        asyncAfter(0.5) { self.GoToLists() }
+                    }}}}}}
     
     private func GoToLists() {
-    let vc = self.storyboard?.instantiateViewController(withIdentifier: "ListsNC")
-    self.definesPresentationContext = true
-    vc?.modalPresentationStyle = .overCurrentContext
-    self.present(vc!, animated: true, completion: nil)
-    /*
-    при успешной авторизации, можно начать подгружать какие-нибудь
-    данные со сторонних ресурсов, например курс и новости
-    */
-    NewsArray()
-    BitcoinRate()
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "ListsNC")
+        self.definesPresentationContext = true
+        vc?.modalPresentationStyle = .overCurrentContext
+        self.present(vc!, animated: true, completion: nil)
+        /*
+         при успешной авторизации, можно начать подгружать какие-нибудь
+         данные со сторонних ресурсов, например курс и новости
+         */
+        NewsArray()
+        BitcoinRate()
     }
 }
