4427aa51bffb837ac835520f95b28781bd4a3c28
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 02:31:38 +0500

    Update GridDelegate.swift

--- IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Lists/Collection FlowLayouts/GridDelegate.swift
+++ IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Lists/Collection FlowLayouts/GridDelegate.swift
@@ -5,38 +5,38 @@ class GridDelegate: ItemsDefGridDelegate {
     private let space: CGFloat = 1
     
     func collectionView(_ collectionView: UICollectionView,
-    layout collectionViewLayout: UICollectionViewLayout,
-    sizeForItemAt indexPath: IndexPath) -> CGSize {
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-    let itemSize: CGSize
-    if indexPath.item % 3 == 0 {
-    let itemWidth = collectionView.bounds.width - (sectionInsets.left + sectionInsets.right)
-    itemSize = CGSize(width: itemWidth, height: 125)
-    } else {
-    // размеры между промежуточных картинок
-    let indent = sectionInsets.left + sectionInsets.right + space * (itemsPerRow - 2)
-    let availableWidth = collectionView.bounds.width - indent
-    let widthPerItem = availableWidth / itemsPerRow
-    itemSize = CGSize(width: availableWidth/2-itemsPerRow, height: widthPerItem)
-    }
-    return itemSize
-    }
-    
-    func collectionView(_ collectionView: UICollectionView,
-    layout collectionViewLayout: UICollectionViewLayout,
-    insetForSectionAt section: Int) -> UIEdgeInsets {
-    return sectionInsets
+        let itemSize: CGSize
+        if indexPath.item % 3 == 0 {
+            let itemWidth = collectionView.bounds.width - (sectionInsets.left + sectionInsets.right)
+            itemSize = CGSize(width: itemWidth, height: 125)
+        } else {
+            // размеры между промежуточных картинок
+            let indent = sectionInsets.left + sectionInsets.right + space * (itemsPerRow - 2)
+            let availableWidth = collectionView.bounds.width - indent
+            let widthPerItem = availableWidth / itemsPerRow
+            itemSize = CGSize(width: availableWidth/2-itemsPerRow, height: widthPerItem)
+        }
+        return itemSize
     }
     
     func collectionView(_ collectionView: UICollectionView,
-    layout collectionViewLayout: UICollectionViewLayout,
-    minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-    return 20
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
     }
     
     func collectionView(_ collectionView: UICollectionView,
-    layout collectionViewLayout: UICollectionViewLayout,
-    minimumInteritemSpacingForSectionAt section: Int) -> CGFloat{
-    return space
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat{
+        return space
     }
 }
