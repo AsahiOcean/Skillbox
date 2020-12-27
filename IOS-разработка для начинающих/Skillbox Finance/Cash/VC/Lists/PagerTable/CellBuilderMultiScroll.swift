c1034fd8224aee13d4e08447a149ace365b0df48
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 02:29:26 +0500

    Update CellBuilderMultiScroll.swift

--- IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Lists/PagerTable/CellBuilderMultiScroll.swift
+++ IOS-разработка для начинающих/Skillbox Finance/Cash/VC/Lists/PagerTable/CellBuilderMultiScroll.swift
@@ -6,12 +6,12 @@ var EventText = [String]()
 public class CellBuilderMultiScroll {
     public static func getFeaturedCell(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "featured", for: indexPath) as? MultiScrollVCFeatured {
-        let realm = try! Realm()
-//        var btc: Results<BitCoinObject>!
-        let btcrate = realm.objects(BitCoinObject.self)
-        cell.setup(title: "Курс BitCoin", subtitle:
-            "\(btcrate.last!.When)\nUSD: \(btcrate.last!.USD)\nEUR: \(btcrate.last!.EUR)\nRUB: \(btcrate.last!.RUB)")
-        return cell
+            let realm = try! Realm()
+            //        var btc: Results<BitCoinObject>!
+            let btcrate = realm.objects(BitCoinObject.self)
+            cell.setup(title: "Курс BitCoin", subtitle:
+                        "\(btcrate.last!.When)\nUSD: \(btcrate.last!.USD)\nEUR: \(btcrate.last!.EUR)\nRUB: \(btcrate.last!.RUB)")
+            return cell
         } else {
             return UICollectionViewCell()
         }
@@ -19,21 +19,21 @@ public class CellBuilderMultiScroll {
     
     public static func getTextCell(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "text", for: indexPath) as? MultiScrollVCText {
-        return cell
+            return cell
         } else {
             return UICollectionViewCell()
         }
     }
     
     public static func getListCell(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
-    let CellIndex = indexPath.row
-    let minicell = MiniCells[CellIndex]
-    if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? MultiScrollVCCell {
-    cell.cellTitle.text = minicell.title
-    cell.cellDescription.text = minicell.description
-    cell.iconView.backgroundColor = UIColor(rgb: minicell.color)
-        return cell
-    } else {
+        let CellIndex = indexPath.row
+        let minicell = MiniCells[CellIndex]
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? MultiScrollVCCell {
+            cell.cellTitle.text = minicell.title
+            cell.cellDescription.text = minicell.description
+            cell.iconView.backgroundColor = UIColor(rgb: minicell.color)
+            return cell
+        } else {
             return UICollectionViewCell()
         }
     }
