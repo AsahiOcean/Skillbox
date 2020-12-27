b6cb52d367f4eea5b981ab8284cf5cfd5daff107
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 02:19:42 +0500

    Update ProductsCollectionViewCell.swift

--- IOS-разработка для начинающих/Skillbox Blackstar Shop/Shop/catalog/ProductsCollectionViewCell.swift
+++ IOS-разработка для начинающих/Skillbox Blackstar Shop/Shop/catalog/ProductsCollectionViewCell.swift
@@ -1,14 +1,9 @@
 import UIKit
-// Skillbox
-// Скиллбокс
 
 class ProductsCollectionViewCell: UICollectionViewCell {
-    
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var mainImage: UIImageView!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var button: UIButton!
-    
-    @IBAction func goToProduct(_ sender: UIButton) {
-    }
+    @IBAction func goToProduct(_ sender: UIButton) { }
 }
