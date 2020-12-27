f5150b9a00b8b6cca024b9d8e4649580a244c7b7
AsahiOcean <61920574+AsahiOcean@users.noreply.github.com>
2020-12-28 01:43:51 +0500

    Update EmbedViewController.swift

--- IOS-разработка для начинающих/7. Переходы и взаимодействие экранов/Homework/третий таб/EmbedViewController.swift
+++ IOS-разработка для начинающих/7. Переходы и взаимодействие экранов/Homework/третий таб/EmbedViewController.swift
@@ -20,11 +20,9 @@ class EmbedViewController: UIViewController {
     @IBAction func ButtonEmbedPurple(_ sender: Any) {
         EmbedDelegator?.ColorMainBackground(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1))
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.layer.backgroundColor = EmbedColorBackground
     }
 }
-// Skillbox
-// Скиллбокс
