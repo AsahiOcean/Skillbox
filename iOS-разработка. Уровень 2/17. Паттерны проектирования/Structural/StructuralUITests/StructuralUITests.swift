90b5f6aa7b8949dfd1273235275ae54c02633fb7
ASAHI OCEAN <asahiocean@icloud.com>
2021-07-02 16:16:19 +0500

    Update StructuralUITests.swift

--- iOS-разработка. Уровень 2/17. Паттерны проектирования/Structural/StructuralUITests/StructuralUITests.swift
+++ iOS-разработка. Уровень 2/17. Паттерны проектирования/Structural/StructuralUITests/StructuralUITests.swift
@@ -1,18 +1,18 @@
 import XCTest
 
 class StructuralUITests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         continueAfterFailure = false
     }
-
+    
     override func tearDownWithError() throws {}
-
+    
     func testExample() throws {
         let app = XCUIApplication()
         app.launch()
     }
-
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
