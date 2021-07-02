cc205600d6787433d77cfaff099b692b3b1465c8
ASAHI OCEAN <asahiocean@icloud.com>
2021-07-02 15:59:56 +0500

    Update AppDelegate.swift

--- iOS-разработка. Уровень 2/10. Maps Apple Google Yandex/Homework/Homework/AppDelegate.swift
+++ iOS-разработка. Уровень 2/10. Maps Apple Google Yandex/Homework/Homework/AppDelegate.swift
@@ -5,21 +5,21 @@ import YandexMapKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, GMSMapViewDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         /*
          https://developers.google.com/maps/documentation/ios-sdk/get-api-key?hl=ru
-        */
+         */
         GMSServices.provideAPIKey("")
         GMSPlacesClient.provideAPIKey("")
         
         /*
          https://tech.yandex.ru/maps/mapkit/doc/3.x/concepts/ios/quickstart-docpage/
-        */
+         */
         YMKMapKit.setApiKey("")
         
         // Язык карты
@@ -27,12 +27,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GMSMapViewDelegate {
         
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 }
