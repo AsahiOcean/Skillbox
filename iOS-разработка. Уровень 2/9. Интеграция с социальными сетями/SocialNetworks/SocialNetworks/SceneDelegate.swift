5f6d367bdc755b2c0f18263a9f59f42114847581
ASAHI OCEAN <asahiocean@icloud.com>
2021-07-02 15:40:54 +0500

    Update SceneDelegate.swift

--- iOS-разработка. Уровень 2/9. Интеграция с социальными сетями/SocialNetworks/SocialNetworks/SceneDelegate.swift
+++ iOS-разработка. Уровень 2/9. Интеграция с социальными сетями/SocialNetworks/SocialNetworks/SceneDelegate.swift
@@ -26,8 +26,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {}
-    func sceneDidBecomeActive(_ scene: UIScene) {
-    }
+    func sceneDidBecomeActive(_ scene: UIScene) {}
 
     func sceneWillResignActive(_ scene: UIScene) {
     }
