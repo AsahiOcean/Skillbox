a27239a79cc0ece843be70aea23714cb1514a1b5
ASAHI OCEAN <asahiocean@icloud.com>
2021-07-02 16:12:40 +0500

    commit

--- iOS-разработка. Уровень 2/17. Паттерны проектирования/Behavioral/BehavioralTests/BehavioralTests.swift
+++ iOS-разработка. Уровень 2/17. Паттерны проектирования/Behavioral/BehavioralTests/BehavioralTests.swift
@@ -2,13 +2,13 @@ import XCTest
 @testable import Behavioral
 
 class BehavioralTests: XCTestCase {
-
+    
     override func setUpWithError() throws {}
-
+    
     override func tearDownWithError() throws {}
-
+    
     func testExample() throws {}
-
+    
     func testPerformanceExample() throws {
         self.measure {}
     }
--- iOS-разработка. Уровень 2/17. Паттерны проектирования/Behavioral/BehavioralUITests/BehavioralUITests.swift
+++ iOS-разработка. Уровень 2/17. Паттерны проектирования/Behavioral/BehavioralUITests/BehavioralUITests.swift
@@ -1,19 +1,18 @@
 import XCTest
 
 class BehavioralUITests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         continueAfterFailure = false
     }
-
-    override func tearDownWithError() throws {
-    }
-
+    
+    override func tearDownWithError() throws {}
+    
     func testExample() throws {
         let app = XCUIApplication()
         app.launch()
     }
-
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
