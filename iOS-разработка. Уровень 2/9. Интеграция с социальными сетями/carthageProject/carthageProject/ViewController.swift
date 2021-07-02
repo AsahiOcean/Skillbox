7ef57c87148abb3e88210ab0fb6e487bc16f766f
ASAHI OCEAN <asahiocean@icloud.com>
2021-07-02 15:50:52 +0500

    Update ViewController.swift

--- iOS-разработка. Уровень 2/9. Интеграция с социальными сетями/carthageProject/carthageProject/ViewController.swift
+++ iOS-разработка. Уровень 2/9. Интеграция с социальными сетями/carthageProject/carthageProject/ViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
         AF.request("https://fish-text.ru/get?format=html&number=1").responseString(completionHandler: { response in
